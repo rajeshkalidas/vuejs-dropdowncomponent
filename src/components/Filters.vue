d7bbb5aae0c31019dcc4bfac8e33b934de7ed520
rajeshkalidas <rajeshkumar.kalidas@gmail.com>
2019-12-04 21:52:23 +0100

    initial commit

---
+++ src/components/Filters.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="filters">
+
+    <div class="filter">
+      <label for="basic-dropdown">Basic dropdown: </label>
+      <select name="basic-dropdown" v-model="selectedFruit">
+        <option>Apple</option>
+        <option>Banana</option>
+        <option>Blueberry</option>
+        <option>Kiwi</option>
+        <option>Pear</option>
+        <option>Pineapple</option>
+        <option>Watermelon</option>
+      </select>
+    </div>
+
+
+    <!-- <div class="filter">
+      <label for="component-dropdown">Component-based dropdown: </label>
+      <dropdown id="component-dropdown" :options="fruitOptions" v-model="selectedFruit"></dropdown>
+    </div> -->
+
+    <div class="filter" style="margin-top:50px">
+      <label for="autocomplete-dropdown">Autocomplete dropdown: </label>
+      <autocomplete-dropdown
+        id="autocomplete-dropdown"
+        :options="fruitOptions"
+        v-model="selectedFruit"
+        placeholder="Enter a fruit name"
+      ></autocomplete-dropdown>
+    </div>
+
+    <div class="result">
+      Selected: <strong>{{ selectedFruit}}</strong>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+
+import AutocompleteDropdown from '@/components/AutocompleteDropdown'
+
+import moment from 'moment'
+
+export default {
+  components: {
+    'autocomplete-dropdown': AutocompleteDropdown
+  },
+
+  data () {
+    return {
+      selectedFruit: 'Apple',
+      fruitOptions: {
+        'Apple': 'Apple',
+        'Banana': 'Banana',
+        'Blueberry': 'Blueberry',
+        'Kiwi': 'Kiwi',
+        'Pear': 'Pear',
+        'Pineapple': 'Pineapple',
+        'Watermelon': 'Watermelon'
+      },
+      selectedDateRange: null
+    }
+  },
+
+  computed: {
+    selectedDateRangeFormatted () {
+      if (this.selectedDateRange) {
+        return moment(this.selectedDateRange.from).format('L LT') + ' - ' + moment(this.selectedDateRange.to).format('L LT')
+      } else {
+        return 'None'
+      }
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.filters {
+  width: 800px;
+  margin: 0 auto;
+}
+
+.filter {
+  text-align: left;
+}
+
+.result {
+  margin: 30px 0;
+  text-align: left;
+}
+
+label {
+  display: block;
+}
+
+</style>
